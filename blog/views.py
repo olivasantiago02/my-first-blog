21962e6d2611ad75d7c36a34f027ac6bd27c5a40
santiago <santipboliva@hotmail.com>
2022-06-21 20:09:47 -0300

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_list(request):
+    post= Post.objects.all()
+    return render(request,'blog/post_list.html',{'posts': post})
